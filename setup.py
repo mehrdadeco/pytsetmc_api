26d7d98efb7589996082bf0f8835f7d1b651346e
mehrdadeco <mehrdad5300@gmail.com>
2025-07-27 22:25:25 +0330

    setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='pytsetmc_api',
+    version='0.1',
+    description='Client for fetching TSETMC market data',
+    author='Mehrad',
+    packages=['pytsetmc_api'],
+)
